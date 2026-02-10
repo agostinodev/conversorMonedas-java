cb01a0f61e6a3c8aff0bfa011ca6d67b54e7d636
Nicolas Javier Agostino <nico.agostino@hotmail.com>
2026-02-10 16:37:15 -0300

    Commit inicial del proyecto

---
+++ src/conversor/modelo/Conversion.java
@@ -0,0 +1,13 @@
+package conversor.modelo;
+
+public record Conversion( String base_code,
+                          String target_code,
+                          double conversion_rate,
+                          String time_last_update_utc,
+                          String time_next_update_utc
+                          )  {
+
+
+
+
+}
---
+++ src/conversor/principal/Main.java
@@ -0,0 +1,56 @@
+package conversor.principal;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        boolean continuar = true;
+
+        while ( continuar ){
+
+            System.out.println("------ MENU ------");
+            System.out.println("1 - Dólar => Peso argentino");
+            System.out.println("2 - Peso argentina => Dólar");
+            System.out.println("3 - Dólar  => Peso brasileño");
+            System.out.println("4 - Peso brasileño => Dólar");
+            System.out.println("5 - Dólar => Peso chileno");
+            System.out.println("6 - Peso chileno => Dólar");
+            System.out.println("7 - Salir");
+            System.out.println("Elija una opción válida:");
+            int opc = sc.nextInt();
+
+
+            switch (opc){
+                case 1:{
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
